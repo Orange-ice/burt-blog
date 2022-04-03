86ffc48d70465c7e28eab040f36aef2575576f56
Burt <2902978956@qq.com>
2022-04-03 13:37:32 +0800

    初始化应用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+  return <>
+    <Head>
+      <title>burt-blog</title>
+      <meta name="description" content="burt的个人博客"/>
+      <meta name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"/>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
-export default MyApp
+export default MyApp;
