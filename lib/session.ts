7e0049ba98df0a5cc7cbeb25163a972d37790d16
Burt <2902978956@qq.com>
2022-04-05 14:09:10 +0800

    引入 iron-session 做登录会话管理

---
+++ lib/session.ts
@@ -0,0 +1,25 @@
+import {withIronSessionApiRoute, withIronSessionSsr} from 'iron-session/next/dist';
+import {NextApiHandler, GetServerSideProps} from 'next';
+import type {IronSessionOptions} from 'iron-session';
+
+const password = '0gyprto1dt7hhe8hyof8pxo522xgghti';
+
+const sessionOptions: IronSessionOptions = {
+  password,
+  cookieName: 'session-burt',
+  cookieOptions: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+    sameSite: 'lax',
+    secure: false,
+  },
+};
+
+export const withSessionApi = (handle: NextApiHandler) => {
+  return withIronSessionApiRoute(handle, sessionOptions);
+};
+
+
+export const withSessionSsr = (handle: GetServerSideProps) => {
+  return withIronSessionSsr(handle, sessionOptions);
+};
