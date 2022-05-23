1fc7b6b707ab2a025a083515a3f27c11bce0ba4e
黄冰 <2902978956@qq.com>
2022-05-23 10:51:24 +0800

    backstage 基本布局

--- pages/admin/backstage.tsx
+++ pages/admin/backstage.tsx
@@ -1,9 +1,20 @@
 import {NextPage} from 'next';
+import {Menu} from '@arco-design/web-react';
+import s from '../../styles/backstage.module.css'
 
 const Backstage: NextPage = () => {
-  return (
-    <h1>backstage</h1>
-  );
+    return (
+        <div className={s.wrapper}>
+            <Menu
+                className={s.menu}
+                theme="dark"
+                defaultSelectedKeys={['list']}
+            >
+                <Menu.Item key="list">博客列表</Menu.Item>
+            </Menu>
+            <main className={s.main}>main</main>
+        </div>
+    );
 };
 
 export default Backstage;
